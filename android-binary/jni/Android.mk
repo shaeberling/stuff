3c69bff2514da76f3be0070c1be8a943052c7c46
Sascha Haeberling <haeberling@google.com>
2015-10-17 17:12:50 -0700

    Add logging to binary.

--- android-binary/jni/Android.mk
+++ android-binary/jni/Android.mk
@@ -4,6 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE    := AndroidBinary
 
 LOCAL_SRC_FILES := helloworld.cc
+LOCAL_LDLIBS    := -llog
 
 include $(BUILD_EXECUTABLE)
 
--- android-binary/jni/helloworld.cc
+++ android-binary/jni/helloworld.cc
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <android/log.h>
 
 int main()
 {
-     printf("Fourty-two\n");
-     return 0;
+  __android_log_print(ANDROID_LOG_INFO, "ABIN", "Fourty-two!");
+  printf("Fourty-two\n");
+  return 0;
 }
 
